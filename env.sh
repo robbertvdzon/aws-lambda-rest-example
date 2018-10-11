c1b4117d3dcdc36ddc7a70c60df774030ed84abf
robbert <robbert@vdzon.com>
2018-10-11 20:51:49 +0200

    refactor to use sparkjava

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-API_NAME="my_java_rest_application_api"
-LAMBDA_FUNCTION_NAME="my_java_rest_application"
+LAMBDA_NAME="my_java_rest_application_api"
+HANDLER_FUNCTION="my.service.StreamLambdaHandler::handleRequest"
+JAR_FILENAME="aws-lambda-rest-example-1.0-SNAPSHOT.jar"
--- src/main/java/my/service/StreamLambdaHandler.java
+++ src/main/java/my/service/StreamLambdaHandler.java
@@ -1,31 +1,39 @@
 package my.service;
 
-import com.amazonaws.serverless.proxy.jersey.JerseyLambdaContainerHandler;
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spark.SparkLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import org.glassfish.jersey.jackson.JacksonFeature;
-import org.glassfish.jersey.server.ResourceConfig;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import static spark.Spark.before;
+import static spark.Spark.get;
 
 
-public class StreamLambdaHandler implements RequestStreamHandler {
-    private static final ResourceConfig jerseyApplication = new ResourceConfig()
-            .packages("my.service")
-            .register(JacksonFeature.class);
-    private static final JerseyLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler
-            = JerseyLambdaContainerHandler.getAwsProxyHandler(jerseyApplication);
+public class StreamLambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+    private SparkLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler =
+            SparkLambdaContainerHandler.getAwsProxyHandler();
+    private boolean initialized = false;
 
-    @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-            throws IOException {
-        handler.proxyStream(inputStream, outputStream, context);
+    public StreamLambdaHandler() throws ContainerInitializationException {
+    }
 
-        // just in case it wasn't closed by the mapper
-        outputStream.close();
+    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
+        if (!initialized) {
+            defineRoutes();
+            initialized = true;
+        }
+        return handler.proxy(awsProxyRequest, context);
+    }
+
+    private void defineRoutes() {
+        Gson gson = new Gson();
+        before((request, response) -> response.type("application/json"));
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        before((request, response) -> response.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT"));
+        get("/", (req, res) -> "Hello world from sparkjava lambda handler!!");
+
     }
 }
